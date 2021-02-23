3224ef748bef06a0753cc21afac273c12d77ce36
Cendikia <cisnatinnov@gmail.com>
2021-02-23 11:45:10 +0700

    Add accesses

--- routes/accesses.js
+++ routes/accesses.js
@@ -1,12 +1,62 @@
 const express = require('express')
 const app = express();
 var _ = require('lodash');
-var moment = require('moment');
+const moment = require('moment');
 
 // display access
 app.get('/', (req, res) => {
 	if (!req.session.email) res.redirect('/login');
-	else res.render('index', {title: 'Akses', page: 'setting/access', session: req.session});
+	else res.render('index', {title: 'Akses', page: 'setting/accesses', session: req.session});
 })
 
-module.exports = app;
+app.get('/lists', (req, res) => {
+	req.getConnection((error, conn) => {
+		if (error) throw error;
+		conn.query("SELECT * FROM accesses ORDER BY id ASC", (err, rows) => {
+			if (err) throw err;
+			let data = [];
+			_.forEach(rows, (o, i) => {
+				data.push({
+					no: i+1,
+					id: o.id,
+					access: o.access,
+					menu: _.split(o.menu, ','),
+					status: (o.status=='1') ? 'Aktif': 'Tidak Aktif',
+					created_at: o.created_at,
+					updated_at: o.updated_at
+				})
+			})
+			res.json({
+				data: data
+			})
+		})
+	})
+})
+
+app.post('/save', (req, res) => {
+	req.getConnection((error, conn) => {
+		if (error) throw error;
+		else {
+			let data = req.body.access, label = req.body.label, query = '';
+			data.menu = _.join(data.menu, ',');
+			data.status = (data.status=='Aktif') ? '1': '0';
+			if (label=='Tambah') {
+				data.created_at = moment().format('YYYY-MM-DD HH:mm:ss');
+				query = 'INSERT INTO accesses SET ?';
+			}
+			else {
+				data.updated_at = moment().format('YYYY-MM-DD HH:mm:ss');
+				query = 'UPDATE accesses SET ? WHERE id = '+data.id;
+			}
+			conn.query(query, data, (err) => {
+				if (err) throw err;
+				else {
+					res.json({
+						result: 'success'
+					})
+				}
+			});
+		}
+	})
+})
+module.exports = app
--- routes/login.js
+++ routes/login.js
@@ -23,33 +23,38 @@ app.post('/signin', (req, res) => {
       }
       else {
         if (rows[0].password==user.password) {
-          let access = getAccess(rows[0].access_id);
-          conn.query("SELECT * FROM persons WHERE user_id = ?", [rows[0].id], (err, results) => {
-            if (err) throw err;
-            req.session.id = rows[0].id;
-            req.session.full_name = rows[0].full_name;
-            req.session.email = rows[0].email;
-            req.session.password = rows[0].password; 
-            req.session.access_id = access.id;
-            req.session.access = access.access;
-            req.session.menu = access.menu;
-            req.session.first_name = results[0].first_name;
-            req.session.middle_name = results[0].middle_name;
-            req.session.last_name = results[0].last_name;
-            req.session.birth_place = results[0].birth_place;
-            req.session.birth_date = results[0].birth_date;
-            req.session.address = results[0].address;
-            req.session.village = results[0].village;
-            req.session.district = results[0].district;
-            req.session.city = results[0].city;
-            req.session.province = results[0].province;
-            req.session.pos_code = results[0].pos_code;
-            req.session.created_at = results[0].created_at;
-            req.session.since = moment(results[0].created_at).format('LLLL');
-            res.json({
-              result: 'success',
-              message: 'Berhasil masuk'
-            })
+          let access = rows[0].access_id;
+          conn.query("SELECT * FROM accesses WHERE id = "+access, (errAcc, accesses) => {
+            if (errAcc) throw errAcc;
+            else {
+              conn.query("SELECT * FROM persons WHERE user_id = ?", [rows[0].id], (err, results) => {
+                if (err) throw err;
+                req.session.id = rows[0].id;
+                req.session.full_name = rows[0].full_name;
+                req.session.email = rows[0].email;
+                req.session.password = rows[0].password; 
+                req.session.access_id = accesses[0].id;
+                req.session.access = accesses[0].access;
+                req.session.menu = _.split(accesses[0].menu, ',');
+                req.session.first_name = results[0].first_name;
+                req.session.middle_name = results[0].middle_name;
+                req.session.last_name = results[0].last_name;
+                req.session.birth_place = results[0].birth_place;
+                req.session.birth_date = results[0].birth_date;
+                req.session.address = results[0].address;
+                req.session.village = results[0].village;
+                req.session.district = results[0].district;
+                req.session.city = results[0].city;
+                req.session.province = results[0].province;
+                req.session.pos_code = results[0].pos_code;
+                req.session.created_at = results[0].created_at;
+                req.session.since = moment(results[0].created_at).format('LLLL');
+                res.json({
+                  result: 'success',
+                  message: 'Berhasil masuk'
+                })
+              })
+            }
           })
         }
         else {
@@ -70,33 +75,6 @@ app.get('/signout', (req, res)=>{
 	})
 })
 
-function getAccess(id) {
-  let access = {};
-  switch (id) {
-    case 1:
-      access = {
-        id: id,
-        access: 'Admin',
-        menu: ['benefits', 'setting']
-      };
-      break;
-    case 2:
-      access = {
-        id: id,
-        access: 'Keuangan',
-        menu: ['reports']
-      };
-      break;
-    default:
-      access = {
-        id: id,
-        access: 'Petugas',
-        menu: ['vehicles']
-      };
-      break;
-  }
-  return access;
-}
 /** 
  * We assign app object to module.exports
  * 
